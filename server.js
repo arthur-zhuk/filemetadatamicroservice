170f725117d8251eb4cf9de68970462be495c314
ug02fast <arthurzhuk@gmail.com>
2016-02-26 11:47:00 -0800

    file meta data service working properly to obtain size

--- server.js
+++ server.js
@@ -9,6 +9,7 @@ var app = express();
 app.use('/controllers', express.static(process.cwd() + '/app/controllers'));
 app.use('/public', express.static(process.cwd() + '/public'));
 app.use('/common', express.static(process.cwd() + '/app/common'));
+
 routes(app);
 
 var port = process.env.PORT || 8080;
