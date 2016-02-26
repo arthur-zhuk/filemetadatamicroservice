a76d0648f5acdc4fe823fb3bcfe912cba013d83e
ug02fast <arthurzhuk@gmail.com>
2016-02-26 11:49:38 -0800

    got rid of dumb stuff

--- app/routes/index.js
+++ app/routes/index.js
@@ -12,10 +12,7 @@ module.exports = function (app) {
 			res.sendFile(path + '/public/index.html');
 		})
 		.post(multer().single('upl'), function(req, res) {
-			console.log(req.body) // form fields
-			console.log(req.file.size) // form files
-
-			res.json(req.file.size);
+			res.json(req.file.size + " <= The size of the chosen file.");
 			res.status(204).end()
 		});
 
